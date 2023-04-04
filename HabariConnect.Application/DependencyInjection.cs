f823827a3f515755d1e17301774a3c4ea7bade1c
Billy <bimganda@gmail.com>
2023-04-04 10:52:21 -0400

    DI for application layer

---
+++ HabariConnect.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using HabariConnect.Application.Commands;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HabariConnect.Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {            
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+            services.AddScoped<IMediator, Mediator>();
+        }
+    }
+}
