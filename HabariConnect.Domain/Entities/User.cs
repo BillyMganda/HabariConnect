7bad627a248da0c4e0876d5a6b785c5ffef551be
Billy <bimganda@gmail.com>
2023-04-04 07:51:57 -0400

    create user entity

---
+++ HabariConnect.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace HabariConnect.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Handle { get; set; } = string.Empty;
+        public byte[] password_hash { get; set; } = new byte[32];
+        public byte[] password_salt { get; set; } = new byte[32];
+        public DateTime CreatedOn { get; set; }
+        public string ForgotPasswordToken { get; set; } = string.Empty;
+    }
+}
