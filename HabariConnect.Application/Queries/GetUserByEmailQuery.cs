493de9f535202420df0f5daa573b96a13530634b
Billy <bimganda@gmail.com>
2023-04-04 14:35:50 -0400

    add query to get user by email

---
+++ HabariConnect.Application/Queries/GetUserByEmailQuery.cs
@@ -0,0 +1,10 @@
+﻿using HabariConnect.Domain.DTOs.User;
+using MediatR;
+
+namespace HabariConnect.Application.Queries
+{
+    public class GetUserByEmailQuery : IRequest<UserGetDto>
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+}
