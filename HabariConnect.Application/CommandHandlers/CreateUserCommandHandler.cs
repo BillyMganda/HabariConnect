2d11121aad19782a361897ed0c629b0693527b1c
Billy <bimganda@gmail.com>
2023-04-04 09:22:40 -0400

    add command handler to create a new user

--- HabariConnect.Application/CommandHandlers/CreateUserCommandHandler.cs
+++ HabariConnect.Application/CommandHandlers/CreateUserCommandHandler.cs
@@ -1,9 +1,48 @@
 ﻿using HabariConnect.Application.Commands;
+using HabariConnect.Domain.Entities;
+using HabariConnect.Domain.Interfaces;
 using MediatR;
 
 namespace HabariConnect.Application.CommandHandlers
 {
-    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Unit>
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, User>
     {
+        private readonly IUserRepository _userRepository;
+        public CreateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            // Check if the user with the same email address already exists.
+            if (await _userRepository.GetByEmailAsync(request.Email) != null)
+            {
+                throw new BadRequestException("User with the same email address already exists.");
+            }
+
+            // Check if the user with the same handle already exists.
+            if (await _userRepository.GetByHandleAsync(request.Handle) != null)
+            {
+                throw new BadRequestException("User with the same handle already exists.");
+            }
+
+            // Hash the password and create a new user.
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Handle = request.Handle,
+                TermsAgreed = request.TermsAgreed
+            };
+            user.SetPassword(request.Password);
+
+            // Add the user to the repository.
+            await _userRepository.CreateAsync(user);
+
+            return user;
+        }
     }
 }
