8e320c5ac06bf36515597adffc4e4cd31176afff
Billy <bimganda@gmail.com>
2023-04-04 09:09:09 -0400

    create interface to manage user

---
+++ HabariConnect.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using HabariConnect.Domain.Entities;
+
+namespace HabariConnect.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid id);
+        Task<User> GetByEmailAsync(string email);
+        Task<User> GetByHandleAsync(string handle);
+        Task CreateAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
