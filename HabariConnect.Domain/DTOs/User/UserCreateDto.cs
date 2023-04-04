f5357c62f66ca1b87662002644b92327348f75af
Billy <bimganda@gmail.com>
2023-04-04 08:05:15 -0400

    add a DTO to create a user

---
+++ HabariConnect.Domain/DTOs/User/UserCreateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HabariConnect.Domain.DTOs.User
+{
+    public class UserCreateDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Handle { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public bool TermsAgreed { get; set; }
+    }
+}
