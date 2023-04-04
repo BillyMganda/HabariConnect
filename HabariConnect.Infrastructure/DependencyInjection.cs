444d1187c983728cd4ba01e942a22907b87fdc1f
Billy <bimganda@gmail.com>
2023-04-04 10:56:30 -0400

    DI for infra layer

---
+++ HabariConnect.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using HabariConnect.Domain.Interfaces;
+using HabariConnect.Infrastructure.Data;
+using HabariConnect.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HabariConnect.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfraServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HabariConnectDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("HabariConnectDatabase")));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+        }
+    }
+}
