8146ba87095177848dcd70b69f379380e002beaf
Billy <bimganda@gmail.com>
2023-04-04 08:16:54 -0400

    mofify dto to include business rules

--- HabariConnect.Domain/DTOs/User/UserCreateDto.cs
+++ HabariConnect.Domain/DTOs/User/UserCreateDto.cs
@@ -1,13 +1,22 @@
-﻿namespace HabariConnect.Domain.DTOs.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HabariConnect.Domain.DTOs.User
 {
     public class UserCreateDto
     {
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required, MinLength(4)]
         public string Handle { get; set; } = string.Empty;
+        [Required, MinLength(6)]
         public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
+        [Required]
         public bool TermsAgreed { get; set; }
     }
 }
