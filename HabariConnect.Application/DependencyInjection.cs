23cebfa3048b1ad95a8990d72a988fa4c66a3839
Billy <bimganda@gmail.com>
2023-04-04 11:04:07 -0400

    update application layer DI to include queries, commands and their handlers

--- HabariConnect.Application/DependencyInjection.cs
+++ HabariConnect.Application/DependencyInjection.cs
@@ -11,6 +11,13 @@ namespace HabariConnect.Application
         {            
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
             services.AddScoped<IMediator, Mediator>();
+            services.AddScoped<IRequestHandler<GetAllCustomerQuery, List<Customer>>, GetAllCustomerHandler>();
+            services.AddScoped<IRequestHandler<GetCustomerByEmailQuery, Customer>, GetCustomerByEmailHandler>();
+            services.AddScoped<IRequestHandler<GetCustomerByIdQuery, Customer>, GetCustomerByIdHandler>();
+            services.AddScoped<IRequestHandler<CreateCustomerCommand, CustomerResponse>, CreateCustomerHandler>();
+            services.AddScoped<IRequestHandler<DeleteCustomerCommand, string>, DeleteCustomerHandler>();
+            services.AddScoped<IRequestHandler<EditCustomerCommand, CustomerResponse>, EditCustomerHandler>();
+            services.AddMediatR(typeof(GetCustomerByEmailHandler).GetTypeInfo().Assembly);
         }
     }
 }
