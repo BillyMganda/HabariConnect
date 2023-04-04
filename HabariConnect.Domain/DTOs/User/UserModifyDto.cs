6feeb5769aaf2d6dab9e21ad6290fbfde7c794e3
Billy <bimganda@gmail.com>
2023-04-04 08:08:37 -0400

    create dto to modify a user

---
+++ HabariConnect.Domain/DTOs/User/UserModifyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HabariConnect.Domain.DTOs.User
+{
+    public class UserModifyDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
