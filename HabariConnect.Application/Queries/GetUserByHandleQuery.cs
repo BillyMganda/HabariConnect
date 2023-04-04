838054740781e476a469552cf141f33c7442bfc3
Billy <bimganda@gmail.com>
2023-04-04 15:16:36 -0400

    create query to get user by handle

---
+++ HabariConnect.Application/Queries/GetUserByHandleQuery.cs
@@ -0,0 +1,10 @@
+﻿using HabariConnect.Domain.DTOs.User;
+using MediatR;
+
+namespace HabariConnect.Application.Queries
+{
+    public class GetUserByHandleQuery : IRequest<UserGetDto>
+    {
+        public string Handle { get; set; } = string.Empty;
+    }
+}
