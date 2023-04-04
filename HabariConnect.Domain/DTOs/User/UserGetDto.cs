aceb328ea43395a22d20b8cc5d39d0fced5c4223
Billy <bimganda@gmail.com>
2023-04-04 08:10:40 -0400

    create dto to get users

---
+++ HabariConnect.Domain/DTOs/User/UserGetDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HabariConnect.Domain.DTOs.User
+{
+    public class UserGetDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Handle { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool TermsAgreed { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
