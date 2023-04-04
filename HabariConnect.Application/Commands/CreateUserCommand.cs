7cea55fc034a7a487a7c60ef813c9819599e97c1
Billy <bimganda@gmail.com>
2023-04-04 09:00:23 -0400

    create a command to create a user

---
+++ HabariConnect.Application/Commands/CreateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using HabariConnect.Domain.DTOs.User;
+using MediatR;
+
+namespace HabariConnect.Application.Commands
+{
+    public class CreateUserCommand : IRequest<Unit>
+    {
+        public UserCreateDto UserCreateDto { get; set; }
+    }
+}
