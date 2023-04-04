c27e305a52690d34042b7e21c2e3fac10951aca0
Billy <bimganda@gmail.com>
2023-04-04 15:20:01 -0400

    add handler for getting user by handle

---
+++ HabariConnect.Application/QueryHandlers/GetUserByHandleQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using HabariConnect.Application.Exceptions;
+using HabariConnect.Application.Queries;
+using HabariConnect.Domain.DTOs.User;
+using HabariConnect.Domain.Interfaces;
+using MediatR;
+
+namespace HabariConnect.Application.QueryHandlers
+{
+    public class GetUserByHandleQueryHandler : IRequestHandler<GetUserByHandleQuery, UserGetDto>
+    {
+        private readonly IUserRepository _userRepository;
+        public GetUserByHandleQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserGetDto> Handle(GetUserByHandleQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByHandleAsync(request.Handle);
+
+            if (user == null)
+            {
+                throw new NotFoundException($"User with handle {request.Handle} not found.");
+            }
+
+            return new UserGetDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Handle = user.Handle,
+                CreatedOn = user.CreatedOn,
+                LastModified = user.LastModified,
+                TermsAgreed = user.TermsAgreed,
+                IsActive = user.IsActive
+            };
+        }
+    }
+}
