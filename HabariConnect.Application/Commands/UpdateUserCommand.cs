1cf7cc64f7c2e854aea5d449b2a2ab69ae926e06
Billy <bimganda@gmail.com>
2023-04-05 12:01:55 -0400

    a command to modify a user

---
+++ HabariConnect.Application/Commands/UpdateUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using HabariConnect.Domain.DTOs.User;
+using MediatR;
+
+namespace HabariConnect.Application.Commands
+{
+    public class UpdateUserCommand : IRequest<UserGetDto>
+    {
+        public UserModifyDto UserModifyDto { get; set; }
+    }
+}
