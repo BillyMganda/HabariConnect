dda562b696c3cf78e462a8bcc5fd85e5c32b2822
Billy <bimganda@gmail.com>
2023-04-05 08:20:19 -0400

    add command to enable user by id

---
+++ HabariConnect.Application/Commands/EnableUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using HabariConnect.Domain.DTOs.User;
+using MediatR;
+
+namespace HabariConnect.Application.Commands
+{
+    public class EnableUserCommand : IRequest<UserGetDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
