4d8709c9fb594a8c0721b0ebb2c54ce27ce6b0d9
Billy <bimganda@gmail.com>
2023-04-05 05:11:56 -0400

    create handler for getting all users query

---
+++ HabariConnect.Application/QueryHandlers/GetAllUsersQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using HabariConnect.Application.Exceptions;
+using HabariConnect.Application.Queries;
+using HabariConnect.Domain.DTOs.User;
+using HabariConnect.Domain.Interfaces;
+using MediatR;
+
+namespace HabariConnect.Application.QueryHandlers
+{
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, List<UserGetDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        public GetAllUsersQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<UserGetDto>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetAllUsersAsync();
+            if (user == null)
+            {
+                throw new NotFoundException($"Users not found.");
+            }
+
+            return (List<UserGetDto>)await _userRepository.GetAllUsersAsync();
+        }
+    }
+}
