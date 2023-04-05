b3ea66045070f50eb51175b1717f16e6a6043fef
Billy <bimganda@gmail.com>
2023-04-05 12:30:02 -0400

    create exception for records already exists in database

---
+++ HabariConnect.Application/Exceptions/RecordExistsException.cs
@@ -0,0 +1,10 @@
+﻿namespace HabariConnect.Application.Exceptions
+{
+    public class RecordExistsException : Exception
+    {
+        public RecordExistsException(string message) : base(message)
+        {
+
+        }
+    }
+}
