1b25fca8d8ba3488cb3df0bb950f98057c6f2e54
Billy <bimganda@gmail.com>
2023-04-05 12:21:42 -0400

    create command handler to delete a user by id

---
+++ HabariConnect.Application/CommandHandlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using HabariConnect.Application.Commands;
+using HabariConnect.Application.Exceptions;
+using HabariConnect.Domain.Interfaces;
+using MediatR;
+
+namespace HabariConnect.Application.CommandHandlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Guid> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByIdAsync(request.Id);
+            if(user == null)
+            {
+                throw new NotFoundException($"user with id {request.Id} not found");
+            }
+
+            await _userRepository.DeleteUserAsync(user);
+
+            return request.Id;
+        }
+    }
+}
