fdb32ae73f7814515afc3c3c90589f20dc9900bb
Billy <bimganda@gmail.com>
2023-04-05 07:20:36 -0400

    create handler for disable user by id command

---
+++ HabariConnect.Application/CommandHandlers/DisableUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using HabariConnect.Application.Commands;
+using HabariConnect.Application.Exceptions;
+using HabariConnect.Domain.DTOs.User;
+using HabariConnect.Domain.Entities;
+using HabariConnect.Domain.Interfaces;
+using MediatR;
+
+namespace HabariConnect.Application.CommandHandlers
+{
+    public class DisableUserCommandHandler : IRequestHandler<DisableUserCommand, UserGetDto>
+    {
+        private readonly IUserRepository _userRepository;
+        public DisableUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserGetDto> Handle(DisableUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByIdAsync(request.Id);
+            if (user == null)
+            {
+                throw new NotFoundException($"User with Id {request.Id} does not exists.");
+            }
+
+            await _userRepository.DisableUserAsync(request.Id);            
+
+            var userGetDto = new UserGetDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Handle = user.Handle,
+                CreatedOn = user.CreatedOn,
+                LastModified = user.LastModified,
+                TermsAgreed = user.TermsAgreed,
+                IsActive = user.IsActive
+            };
+
+            return userGetDto;
+        }
+    }
+}
