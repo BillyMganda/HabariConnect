4506f48b675e37df9588ea21220afa9b2af6083b
Billy <bimganda@gmail.com>
2023-04-05 12:07:25 -0400

    create a command handler to update a user

---
+++ HabariConnect.Application/CommandHandlers/UpdateUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using HabariConnect.Application.Commands;
+using HabariConnect.Application.Exceptions;
+using HabariConnect.Domain.DTOs.User;
+using HabariConnect.Domain.Interfaces;
+using MediatR;
+
+namespace HabariConnect.Application.CommandHandlers
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserGetDto>
+    {
+        private readonly IUserRepository _userRepository;
+        public UpdateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserGetDto> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByIdAsync(request.UserModifyDto.Id);
+            if (user == null)
+            {
+                throw new NotFoundException($"User with id {request.UserModifyDto.Id} does not exists.");
+            }
+
+            await _userRepository.UpdateUserAsync(user);
+
+            var userGetDto = new UserGetDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Handle = user.Handle,
+                CreatedOn = user.CreatedOn,
+                LastModified = user.LastModified,
+                TermsAgreed = user.TermsAgreed,
+                IsActive = user.IsActive
+            };
+
+            return userGetDto;
+        }
+    }
+}
