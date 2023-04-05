07c68cf86b12ab6af49ceb28074a5acd921dd95b
Billy <bimganda@gmail.com>
2023-04-05 15:01:07 -0400

    add query handler for searching users by FirstName and LastName

--- HabariConnect.Application/Queries/SearchUsersQuery.cs
+++ HabariConnect.Application/Queries/SearchUsersQuery.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace HabariConnect.Application.Queries
 {
-    public class SearchUsersQuery : IRequest<UserGetDto>
+    public class SearchUsersQuery : IRequest<IEnumerable<UserGetDto>>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
---
+++ HabariConnect.Application/QueryHandlers/SearchUsersQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using HabariConnect.Application.Queries;
+using HabariConnect.Domain.DTOs.User;
+using HabariConnect.Domain.Interfaces;
+using MediatR;
+
+namespace HabariConnect.Application.QueryHandlers
+{
+    public class SearchUsersQueryHandler : IRequestHandler<SearchUsersQuery, IEnumerable<UserGetDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        public SearchUsersQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        public async Task<IEnumerable<UserGetDto>> Handle(SearchUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.SearchUsersAsync(request.FirstName, request.LastName);
+
+            var userGetDtos = users.Select(user => new UserGetDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Handle = user.Handle,
+                CreatedOn = user.CreatedOn,
+                LastModified = user.LastModified,
+                TermsAgreed = user.TermsAgreed,
+                IsActive = user.IsActive
+            });
+
+            return userGetDtos;
+        }
+    }
+}
