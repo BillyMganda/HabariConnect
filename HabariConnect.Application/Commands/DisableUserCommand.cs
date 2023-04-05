908ff72adfa36d99828e669a2357ff438c2f035e
Billy <bimganda@gmail.com>
2023-04-05 07:13:11 -0400

    create a command to disable a user by its id

---
+++ HabariConnect.Application/Commands/DisableUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using HabariConnect.Domain.DTOs.User;
+using MediatR;
+
+namespace HabariConnect.Application.Commands
+{
+    public class DisableUserCommand : IRequest<UserGetDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
